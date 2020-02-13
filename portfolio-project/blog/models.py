61a6eddc98adafba2e2e4375177165ee03a593a5
Osskr <osskrjuarez@gmail.com>
2020-02-13 18:14:44 -0300

    Post y home page up and running

--- portfolio-project/blog/models.py
+++ portfolio-project/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class BlogPost(models.Model):
+
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(default='default.jpg', upload_to='images/')
